7d178d142ca088d3c2ef5e45beb2b8592b36d818
michaelmu <michael.musson@gmail.com>
2023-01-15 18:44:02 -0800

    Test deploy

--- myq.py
+++ myq.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     return {
         'statusCode': 200,
-        'body': "The time is: " + current_date
+        'body': "The time is:   " + current_date
     }
