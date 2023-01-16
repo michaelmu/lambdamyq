63a5009046f4610bc49602204da4bcf0b3f6c1e2
michaelmu <michael.musson@gmail.com>
2023-01-15 18:24:49 -0800

    Update workflow

--- myq.py
+++ myq.py
@@ -1,5 +1,5 @@
 
-def lambda_handler(event, context):
+def lambda_function(event, context):
     current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     return {
         'statusCode': 200,
