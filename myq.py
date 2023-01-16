5360d15e18eb5c8badd85397665a03d8a3da5447
michaelmu <michael.musson@gmail.com>
2023-01-15 17:41:33 -0800

    Updates

--- myq.py
+++ myq.py
@@ -1,1 +1,7 @@
-# test
+
+def lambda_handler(event, context):
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return {
+        'statusCode': 200,
+        'body': "Tthe time is: " + current_date
+    }
