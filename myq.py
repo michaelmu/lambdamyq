d6863a208205854e9810dcb9ed0d2a47ddf47d56
michaelmu <michael.musson@gmail.com>
2023-01-15 18:37:46 -0800

    Fix typo

--- myq.py
+++ myq.py
@@ -1,7 +1,7 @@
 
-def lambda_function(event, context):
+def lambda_handler(event, context):
     current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     return {
         'statusCode': 200,
-        'body': "Tthe time is: " + current_date
+        'body': "The time is: " + current_date
     }
